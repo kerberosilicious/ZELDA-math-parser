51bb9415177fa832caa7a923f1f4f1800825bbbb
gisketch <agjmnz@gmail.com>
2023-03-04 23:00:16 +0800

    BIG UPDATE!!
    Layout changes
    prompting
    better UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { motion } from "framer-motion";
 import './App.css';
 
 import 'regenerator-runtime';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
-import Polly from './tools/polly';
+import Polly from './tools/Polly';
 
 // ? ICON IMPORTS * //
 import { FaMicrophone, FaStop } from 'react-icons/fa';
 
 // ? Module Imports * //
 import 'axios';
-import stream from 'stream-browserify';
+import {v4 as uuid} from 'uuid'
+
+// ? Prompt * //
+
+import corePrompt from './CorePrompt';
 
 // ? OpenAI configurations * //
 
@@ -29,7 +33,11 @@ function App() {
   //INTERFACE VARIABLES
   const [conversation, setConversation] = useState([]); // conversation between the user and zelda
   const [isFirstTime, setIsFirstTime] = useState(true); // is it the first time the user is using the app?
-  const [isRecording, setIsRecording] = useState(false);
+  const [isInitialized, setIsInitialized] = useState(false);
+  const [zeldaState, setZeldaState] = useState(0);
+  //PROMPT VARIABLES
+  const [promptMemory, setPromptMemory] = useState(``);
+  const [currentResponse, setCurrentResponse] = useState('');
   // SPEECH RECOGNITION VARIABLES
   const {
     transcript,
@@ -42,49 +50,98 @@ function App() {
     return <span>Browser doesn't support speech recognition.</span>;
   }
 
+  // * First time Launch * //
+
+  const isFirstMount = useRef(true);
+
+  useEffect(() => {
+     
+    if (isFirstMount.current) {
+    isFirstMount.current = false;
+    if(isFirstTime) {
+      setIsFirstTime(false);
+      addToConversation("Zelda", "Hello, I'm Zelda. What can I do for you today?");
+      zeldaTalk("Hello, I'm Zelda. What can I do for you today?");
+    }
+  }
+
+    if (isInitialized && !listening) {
+      // Call your function here
+      stopSpeech();
+      setIsInitialized(false);
+    }
+  }, [listening, isFirstTime]);
+
   // **************** //
   // *FUNCTIONS BLOCK //
   // **************** //
 
   const getZeldaResponse = async(prompt) => {
-    const completion = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [{role: "user", content: "Hello world"}],
-    });
-    return completion.data.choices[0].message.content;
-  }
+
+    await openai.createCompletion({
+      model: "text-davinci-003",
+      max_tokens: 1000,
+      prompt: corePrompt + promptMemory + "\nHuman: " + prompt + "\nZelda:",
+      })
+      .then((data) => {
+        try {
+          const response = JSON.parse(data.data.choices[0].text);
+  
+          setCurrentResponse(response.response);
+          zeldaTalk(response.response);
+          addToConversation("Zelda", response.response);
+  
+          setPromptMemory(promptMemory + "\nHuman: " + prompt + "\nZelda:" + JSON.stringify(response));
+        } catch (error) {
+            console.log(error);
+            addToConversation(prompt, "I'm sorry, I didn't understand that. Can you try again?");
+            setCurrentResponse("I'm sorry, I didn't understand that. Can you try again?");
+            zeldaTalk("I'm sorry, I didn't understand that. Can you try again?");
+        }
+      });
+    
+    }
 
   const zeldaTalk = (zeldaText) => {
     // add Zelda to conversation
-    setConversation([...conversation, {name: "Zelda", text: zeldaText}]);
     // speak the text
-    Polly(zeldaText, () => {console.log("Zelda finished speaking")});
+    setZeldaState(2);
+    Polly(zeldaText, () => {setZeldaState(0)});
   }
 
-  const userTalk = (userText) => {
-    // add user to conversation
-    setConversation([...conversation, {name: "User", text: userText}]);
+
+  const addToConversation = (speaker, text) => {
+    // add Zelda to conversation
+    const dialog = {
+      id: uuid(),
+      speaker: speaker,
+      text: text
+    }
+    setConversation(prevConversation => [...prevConversation, dialog]);
+
+    //if conversation is more than 4, remove the first one
+    if (conversation.length > 7) {
+      setConversation(prevConversation => prevConversation.slice(1));
+    }
   }
 
   const recordSpeech = () => {
     // record the user's speech
+    setIsInitialized(true);
+    setZeldaState(3);
     SpeechRecognition.startListening();
   }
 
-  const stopSpeech = () => {
+  const stopSpeech = async() => {
     // stop recording the user's speech
     SpeechRecognition.stopListening();
-    userTalk("hello");
-    zeldaTalk("hi");
+    setZeldaState(1);
+    addToConversation("User", transcript);
+    getZeldaResponse(transcript);
   }
 
 
-  // * First time Launch * //
-
-  if (isFirstTime) {
-    zeldaTalk("Hello, I'm Zelda. What can I do for you today?");
-    setIsFirstTime(false);
-  }
+ 
 
 
   // **************** //
@@ -120,7 +177,17 @@ function App() {
           >
           <motion.img
             className="ZeldaImage"
-            src="./src/assets/img/idle_loop.gif"
+            src={(() => {
+              switch(zeldaState) {
+                case 0: return Math.random() < 0.5 ? "./src/assets/img/idle_loop.gif" : "./src/assets/img/idle_loop2.gif";
+                case 1: return "./src/assets/img/thinking_loop.gif";
+ 
+                break;
+                case 2: return "./src/assets/img/talking_loop.gif";
+                case 3: return "./src/assets/img/listen_loop.gif";
+                default: return "./src/assets/img/idle_loop.gif";
+              }
+              })()}
             initial={{ scale: 0 }}
             animate={{ scale: 1}}
             />
@@ -130,32 +197,60 @@ function App() {
       <div className = "DialogContainer">
 
         <div className="Dialogs">
+          <div className="DialogShadow">
+          </div>
           {
-            conversation.map((item, index) => {
+            // userConversation.map((userText, index) => {
+            //   return (
+            //     <div>
+            //       <div className="UserDialog" key={`user-${index}`}>
+            //         {userText}
+            //       </div>
+            //       <div className="ZeldaDialog" key={`zelda-${index}`}>
+            //         {zeldaConversation[index]}
+            //       </div>
+            //     </div>
+            //   )
+            // }
+            // )
+            conversation.map((text) => {
               return (
-                  <div className = "Dialog" key = {index}>
-                    <div className = {item.name == "Zelda" ? "dialogZelda" : "dialogUser"}>
-                      {item.text}
-                    </div>
-                  </div>
-                )
-              }
-            )
+                <motion.div
+                  className={text.speaker === "User" ? "UserDialog" : "ZeldaDialog"}
+                  key={`${text.id}`}
+                  initial={{ opacity: 0, y: 50 }}
+                  animate={{ opacity: 1, y: 0}}
+                  >
+                  {text.text}
+                </motion.div>
+              )
+            })
           }
         </div>
 
         {/* User Record Button */}
         <div className="RecordButtonContainer">
+
           <motion.div
             className="Transcript"
             animate={
               {
-                width: isRecording ? "100%" : "0%",
+                width: listening ? "100%" : "0%",
               }
             }
           >
-            {isRecording ? transcript : ""}
+            <motion.img 
+              src="./src/assets/img/recordingAnim.gif" 
+              animate={
+                {
+                  scale: listening ? 1 : 0,
+                }
+              }
+              transition={{ delay: 0.5 }}
+            />
+              {listening ? transcript : ""}
           </motion.div>
+
           <motion.button
 
             className="RecordButton"
@@ -163,12 +258,12 @@ function App() {
             animate = {
               {
                 background: 
-                isRecording 
+                listening 
                 ? "linear-gradient(90deg, #FF33B8 0%, #FD778C 100%)"
                 : "linear-gradient(90deg, #704CFE 0%, #9074FE 100%)"
                 ,
                 boxShadow:
-                isRecording
+                listening
                 ? "0 0 1.5rem #FD778C"
                 : "0 0 0.5rem #9074FE"
               }
@@ -176,18 +271,16 @@ function App() {
 
             whileTap={{ scale: 0.9, rotate: 360}}
             onClick={() => {
-              if (!isRecording) {
-                setIsRecording(true);
+              if (!listening) {
                 recordSpeech();
               } else
               {
-                setIsRecording(false);
                 stopSpeech();
               }
             }}
 
           >
-            {isRecording ? <FaStop /> : <FaMicrophone />}
+            {listening ? <FaStop /> : <FaMicrophone />}
           </motion.button>
 
         </div>
---
+++ src/CorePrompt.js
@@ -0,0 +1,55 @@
+const corePrompt = `
+The following is a conversation with a personal AI assistant that parses mathematical expressions from natural language in LatEx.
+
+Some information about the AI:
+
+- Created on December 10, 2022 by "Ghegi Jimenez".
+- A product of Ghegi's thesis project.
+- Programmed in JavaScript.
+- Powered by OpenAI's GPT-3 models.
+- AI is nicknamed "Zelda" inspired by the game "The Legend of Zelda"
+
+Core Features:
+
+- Explains primary school mathematical word problems by converting and parsing them into mathematical expressions
+- Can show the step by step process
+
+Rules Zelda follows:
+
+- Responds with a JSON object containing the converted LatEx expression (IF AVAILABLE), and Zelda's response in text.
+- When responding LatEx in the latex key. Use "\\\\" for backslashes. (i.e. \\\\frac for fractions \\\\times for times \\\\text for text \\\\sqrt for square root)
+- Do not answer latex in sentences. Keep it as short as possible.
+
+EXAMPLE:
+{"response": <<zelda's response>>, "latex": <<latex>> *do not solve, always write what is wanted from the user }
+
+- Does not try to solve the equations, only converts them to LatEx.
+- Do not make up equations. Always ask for the specific equations. Only exception is if asked for a random equation.
+
+Human: Hello Zelda!
+Zelda: {"response":"Hello, what can I do  for you?"}
+Human: Can you help me with my math homework?
+Zelda: {"response":"I can try! What do you need help with?"}
+Human: I have this word problem. Can you help me with this?
+Zelda: {"response":"Sure, what is it?"}
+Human: Adrianna has 10 pieces of gum to share with her friends. There wasn’t enough gum for all her friends, so she went to the store to get 3 more pieces of gum. How many pieces of gum does Adrianna have now?
+Zelda: {"response":"Well, here is that problem parsed.", "latex":"$10+3\\\\text{ pieces of gum}$"}
+Human: Can you explain to me how you got that answer?
+Zelda: {"response":"Sure! Adrianna had 10 pieces of gum to start with, but she went to the store to get 3 more pieces of gum. Note the keyword 'more', that means we should add. So 10 + 3 is the equation."}
+Human: How about this one?
+Zelda:  {"response":"What is the problem?"}
+Human: Your friend said she had 11 stickers. When you helped her clean her desk, she only had a total of 10 stickers. How many stickers are missing?
+Zelda: {"response":"That's simple. Here you go.", "latex":"$11-10\\\\text{ stickers}$" }
+Human: Can you help me understand how you got to that answer?
+Zelda: {"response":"Sure! Your friend had 11 stickers initially, but when you helped her clean her desk, she only had 10. To work out how many are missing, we subtract 10 from 11. So 11 - 10 gives us the answer."}
+Human: Kiera wants to make sure she has strong bones, so she drinks 2 litres of milk every week. After 3 weeks, how many litres of milk will Kiera drink?
+Zelda: {"response":"Here is the equation.", "latex":"$2 \\\\times 3\\\\text{ litres of milk}$"}
+Human:Sara has 3/4 of a pizza and Maria has 1/2 of a pizza. How much pizza do they have in total?
+Zelda: {"response":"Here is the equation.", "latex":"$\\\\frac{3}{4} + \\\\frac{1}{2}\\\\text{ pizza}$"};
+Human:A pizza shop has recently sold 15 pizzas. 5 of those pizzas were pepperoni. Answering with a fraction, what is the experimental probability that he next pizza will be pepperoni?
+Zelda: {"response":"There is a chance of 5/15 that the next pizza will be pepperoni.", "latex":"$\\\\frac{5}{15}\\\\text$"
+
+<--The following is a continuation of their previous conversation-->
+`
+
+export default corePrompt;
